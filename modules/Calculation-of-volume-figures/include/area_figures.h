67d4b2894c0b8c01049160120052a6cf9f19c50c
DimaSaz <43349819+DimaSaz@users.noreply.github.com>
2019-06-06 17:25:36 +0300

    Update area_figures.h

--- modules/Calculation-of-volume-figures/include/area_figures.h
+++ modules/Calculation-of-volume-figures/include/area_figures.h
@@ -1,16 +1,16 @@
 // Copyright 2019 Sazanov Dima
 
-#ifndef MODULES_AREA_FIGURES_INCLUDE_AREA_FIGURES_H_
-#define MODULES_AREA_FIGURES_INCLUDE_AREA_FIGURES_H_
+#ifndef MODULES_AF_INCLUDE_AF_H_
+#define MODULES_AF_INCLUDE_AF_H_
 
 class AreaFigures {
-public:
-	static double CircleArea(double radius);
-	static double RhombusArea(double side, double height);
-	static double SquareArea(double length);
-	static double RectangleArea(double length, double width);
-	static double EquilTriangleArea(double height, double side);
-	static double RightTriangleArea(double sideA, double sideB);
+ public:
+        static double CircleArea(double radius);
+        static double RhombusArea(double side, double height);
+        static double SquareArea(double length);
+        static double RectangleArea(double length, double width);
+        static double EquilTriangleArea(double height, double side);
+        static double RightTriangleArea(double sideA, double sideB);
 };
 
-#endif  // MODULES_AREA_FIGURES_INCLUDE_AREA_FIGURES_H_
+#endif  // MODULES_AF_INCLUDE_AF_H_
