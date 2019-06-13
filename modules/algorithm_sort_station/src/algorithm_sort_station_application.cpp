0f92173855e27cd8bc444523a2799bb07fd10bdb
DimaSaz <43349819+DimaSaz@users.noreply.github.com>
2019-06-13 15:10:43 +0300

    Update algorithm_sort_station_application.cpp

--- modules/algorithm_sort_station/src/algorithm_sort_station_application.cpp
+++ modules/algorithm_sort_station/src/algorithm_sort_station_application.cpp
@@ -8,44 +8,43 @@
 #include <string>
 #include "include/shapes_area_application.h"
 
- std::string AlgSortStationApplication::operator()(int argc, const char **argv) {
-	std::string output = "result = ";
-	if (argc == 1) {
-		return Info();
-	}
-	try {
-		output += ParseCommand(argc, argv);
-	}
-	catch (std::runtime_error &exp) {
-		return exp.what();
-	}
-	catch (std::string &exp) {
-		return exp;
-	}
-	return output;
+std::string AlgSortStationApplication::operator()
+        (int argc, const char **argv) {
+        std::string output = "result = ";
+        if (argc == 1) {
+                return Info();
+        }
+        try {
+                output += ParseCommand(argc, argv);
+        }
+        catch (std::runtime_error &exp) {
+                return exp.what();
+        }
+        catch (std::string &exp) {
+                return exp;
+        }
+        return output;
 } 
 
 std::string AlgSortStationApplication::Info() {
-	return "Welcome to the program -Sorting Station Algorithm-\n"
-		"To continue, enter the formula\n"
-		"!!!The formula can contain only integers, brackets, and arithmetic signs!!!\n"
-		"!!!The formula should be written without spaces!!!\n";
+        return "Welcome to the program -Sorting Station Algorithm-\n"
+                "To continue, enter the formula\n"
+                "!!!The formula can contain only integers, brackets, and arithmetic signs!!!\n"
+                "!!!The formula should be written without spaces!!!\n";
 }
 
 std::string AlgSortStationApplication::ParseCommand(int argc, const char **arg) {
-	if (strcmp(arg[1], "info") == 0) {
-		return Info();
-	}
-	else if (argc == 2) {
-		return ASSF(argc, arg);
-	}
-	else {
-		throw std::runtime_error("Input Error");
-	}
+        if (strcmp(arg[1], "info") == 0) {
+                return Info();
+        } else if (argc == 2) {
+                return ASSF(argc, arg);
+        } else {
+                throw std::runtime_error("Input Error");
+        }
 }
 
 std::string AlgSortStationApplication::ASSF(int argc, const char** arg) {
-	AlgSortStationFormula::AlgSortStationFormula(arg[2]);
-	return std::to_string(AlgSortStationFormula::FormulaCalculator());
+        AlgSortStationFormula::AlgSortStationFormula(arg[2]);
+        return std::to_string(AlgSortStationFormula::FormulaCalculator());
 }
 
