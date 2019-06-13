2a1ebcdaf7fd407bf6ccce09c9ebb90b1000d81d
DimaSaz <43349819+DimaSaz@users.noreply.github.com>
2019-06-13 12:48:14 +0300

    Update algorithm_sort_station_application.cpp

--- modules/algorithm_sort_station/application/algorithm_sort_station_application.cpp
+++ modules/algorithm_sort_station/application/algorithm_sort_station_application.cpp
@@ -1,10 +1,15 @@
 // Copyright 2019 Sazanov Dima
 
+#include <cstdlib>
+#include <cstdio>
+#include <string>
 #include <iostream>
+
 #include "include/alg_sort_station_formula_application.h"
 
-int main(int argc, char const** argv) {
-	ASSFApplication application;
-	std::cout << application(argc, argv);
-	return 0;
+int main(int argc, const char** argv) {
+    ASSFApplication app;
+    std::string output = app(argc, argv);
+    std::cout << output.c_str() << std::endl;
+    return 0;
 }
